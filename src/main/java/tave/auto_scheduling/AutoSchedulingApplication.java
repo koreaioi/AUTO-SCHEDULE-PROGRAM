caebfcaac36a4b8a54e30885f0c21351a97466ad
koreaioi <koreaioi@gachon.ac.kr>
2025-07-24 16:22:22 +0900

    Build: 초기 세팅

---
+++ src/main/java/tave/auto_scheduling/AutoSchedulingApplication.java
@@ -0,0 +1,13 @@
+package tave.auto_scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSchedulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tave/auto_scheduling/AutoSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package tave.auto_scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
