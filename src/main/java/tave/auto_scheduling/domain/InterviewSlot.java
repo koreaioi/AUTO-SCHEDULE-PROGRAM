e6e6a6e99d068a91b8a75651c970e20637795b2e
koreaioi <koreaioi@gachon.ac.kr>
2025-07-25 03:32:14 +0900

    feat: InterviewSlot 추가

---
+++ src/main/java/tave/auto_scheduling/domain/InterviewSlot.java
@@ -0,0 +1,23 @@
+package tave.auto_scheduling.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+/*
+* 면접 가능 날짜 + 시간 단위를 나타냄.
+* @PlanningVariable, @ValueRangeProvider에서 사용하기 위해 객체화를 거침
+* */
+
+@Getter
+@Builder
+public class InterviewSlot {
+    private LocalDateTime time;
+
+    public InterviewSlot of(LocalDateTime time) {
+        return InterviewSlot.builder()
+                .time(time)
+                .build();
+    }
+}
