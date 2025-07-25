39040cc84472e1f943a7e41ea858a4157e16c39b
koreaioi <koreaioi@gachon.ac.kr>
2025-07-25 21:16:58 +0900

    feat: Soft 제약 조건 추가

--- src/main/java/tave/auto_scheduling/constraint/InterviewConstraintProvider.java
+++ src/main/java/tave/auto_scheduling/constraint/InterviewConstraintProvider.java
@@ -12,9 +12,12 @@ public class InterviewConstraintProvider implements ConstraintProvider {
     @Override
     public Constraint[] defineConstraints(ConstraintFactory factory) {
         return new Constraint[] {
+                mustBeAvailableSlot(factory),
                 maxThreePerSlot(factory),
                 limitDistinctParts(factory),
-                mustBeAvailableSlot(factory),
+                preferSamePartInSlot(factory),
+                preferFullInterviewSlots(factory),
+                bonusForSinglePartSlot(factory)
         };
     }
 
@@ -47,6 +50,34 @@ public class InterviewConstraintProvider implements ConstraintProvider {
                 .filter((slot, partSet) -> partSet.size() > 3)
                 .penalize(HardSoftScore.ONE_HARD, (slot, partSet) -> partSet.size() - 3)
                 .asConstraint("한 시간대에 허용된 파트 수(3개)를 초과");
+    }
+
+
+    // [Soft] 제약 1 - 같은 파트일수록 보상
+    private Constraint preferSamePartInSlot(ConstraintFactory factory) {
+        return factory.forEachUniquePair(ApplicantAssignment.class,
+                        Joiners.equal(ApplicantAssignment::getAssignedSlot))
+                .filter((a1, a2) -> !a1.getApplicant().getPart().equals(a2.getApplicant().getPart()))
+                .penalize(HardSoftScore.ONE_SOFT)
+                .asConstraint("같은 파트일수록 선호도 증가");
+    }
+
+    // [Soft] 제약 2- 한 면접 시간에 사람이 많을 수록 더 높은 선호도 (높은 점수를 부여함.) 단 하드 때문에 3명은 안넘음!
+    private Constraint preferFullInterviewSlots(ConstraintFactory factory) {
+        return factory.forEach(ApplicantAssignment.class)
+                .groupBy(ApplicantAssignment::getAssignedSlot, ConstraintCollectors.count())
+                .reward(HardSoftScore.ofSoft(2), (slot, count) -> count * count)
+                .asConstraint("한 시간대에 인원이 많을수록 선호도 증가");
+    }
+
+    // [Soft] 제약 3 - 같은 쌍만 있을 경우 가중치
+    private Constraint bonusForSinglePartSlot(ConstraintFactory factory) {
+        return factory.forEach(ApplicantAssignment.class)
+                .groupBy(ApplicantAssignment::getAssignedSlot,
+                        ConstraintCollectors.toSet(a -> a.getApplicant().getPart()))
+                .filter((slot, partSet) -> partSet.size() == 1)
+                .reward(HardSoftScore.ONE_SOFT)
+                .asConstraint("한 시간대에 동일 파트만 있으면 추가 보상");
     }
 
 }
