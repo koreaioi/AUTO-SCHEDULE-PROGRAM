fba3a44b66e1ecce57ecfdcbe64c53a1076fcba7
koreaioi <koreaioi@gachon.ac.kr>
2025-07-25 21:12:34 +0900

    feat: OptaPlannerConfig 개행 추가

--- src/main/java/tave/auto_scheduling/config/OptaPlannerConfig.java
+++ src/main/java/tave/auto_scheduling/config/OptaPlannerConfig.java
@@ -39,4 +39,5 @@ public class OptaPlannerConfig {
     public SolverManager<InterviewSchedule, UUID> solverManager(SolverFactory<InterviewSchedule> solverFactory) {
         return SolverManager.create(solverFactory);
     }
+
 }
