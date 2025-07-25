e95e25224c454e59f413362b0497461b9fe7f8a9
koreaioi <koreaioi@gachon.ac.kr>
2025-07-25 17:42:27 +0900

    feat: OptaPlannerConfig 추가

---
+++ src/main/java/tave/auto_scheduling/config/OptaPlannerConfig.java
@@ -0,0 +1,42 @@
+package tave.auto_scheduling.config;
+
+import org.optaplanner.core.api.solver.SolverFactory;
+import org.optaplanner.core.api.solver.SolverManager;
+import org.optaplanner.core.config.score.director.ScoreDirectorFactoryConfig;
+import org.optaplanner.core.config.solver.SolverConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tave.auto_scheduling.constraint.InterviewConstraintProvider;
+import tave.auto_scheduling.domain.ApplicantAssignment;
+import tave.auto_scheduling.domain.InterviewSchedule;
+
+import java.util.List;
+import java.util.UUID;
+
+@Configuration
+public class OptaPlannerConfig {
+
+    @Bean
+    public SolverConfig solverConfig() {
+        SolverConfig config = new SolverConfig();
+
+        config.setSolutionClass(InterviewSchedule.class);
+        config.setEntityClassList(List.of(ApplicantAssignment.class));
+        config.setScoreDirectorFactoryConfig(
+                new ScoreDirectorFactoryConfig()
+                        .withConstraintProviderClass(InterviewConstraintProvider.class)
+        );
+
+        return config;
+    }
+
+    @Bean
+    public SolverFactory<InterviewSchedule> solverFactory(SolverConfig solverConfig) {
+        return SolverFactory.create(solverConfig);
+    }
+
+    @Bean
+    public SolverManager<InterviewSchedule, UUID> solverManager(SolverFactory<InterviewSchedule> solverFactory) {
+        return SolverManager.create(solverFactory);
+    }
+}
